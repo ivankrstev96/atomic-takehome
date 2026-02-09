221ca1eb8b02c5b931ef4edd91d2bd15351a1c72
ivankrstev96 <ivan96krstev@gmail.com>
2026-02-09 14:40:02 +0100

    Initial setup of Multi-Project Gradle build
    - Spring Boot backend subproject with h2 db
    - Vite React TypeScript subproject
    - hooked gradle clean and build tasks with npm

---
+++ backend/src/main/java/com/ivankrstev/atomictakehome/AtomicTakeHomeApplication.java
@@ -0,0 +1,13 @@
+package com.ivankrstev.atomictakehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtomicTakeHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtomicTakeHomeApplication.class, args);
+	}
+
+}
